6d70a8c3ce1ed0b40805ac7195fe656ad5249d64
Mathew Roichman <roichmankill@gmail.com>
2021-10-24 15:50:31 -0300

    ADD: Tipo de dado boolean

---
+++ tipo-boolean/app.js
@@ -0,0 +1,12 @@
+/*
+Boolean é um tipo de dado que indica se uma expressão é verdadeira (true) ou falsa (false).
+
+- Diferença entre Boolean e boolean
+Boolean: do tipo Object;
+boolean: tipo primitivo.
+É fortemente recomendado que se use o tipo primitivo.
+*/
+var bool1 = true;
+var bool2 = false;
+var bool3 = 4 > 3;
+console.log(bool1, bool2, bool3);
